950d9eb43686a86ef898de530567d89302cf109c
DoctorStrange96 <kamin4ridev@gmail.com>
2020-09-02 12:19:21 -0300

    Update build script
    Signed-off-by: DoctorStrange96 <kamin4ridev@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Initial set-up
-export PATH="$HOME/Toolchains/Linaro-5.5/bin:$PATH";
+export PATH="$HOME/Toolchains/Linaro-5.5-arm-linux-gnueabihf/bin:$PATH";
 export ARCH="arm";
 export SUBARCH="arm";
 export CROSS_COMPILE="arm-linux-gnueabihf-";	
